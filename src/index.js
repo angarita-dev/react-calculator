156a329948ecbe2cfc6f8ac790154845ca1d5574
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-14 10:55:03 -0500

    Added components for styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './styles/index.scss';
+import './styles/main.scss';
 
 // Import Components
 import App from './components/App';
