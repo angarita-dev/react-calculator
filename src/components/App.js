92cdd28d294e4218bae99b48e223df6ba4838a1c
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-17 02:21:19 -0500

    Imported & implemented event listener to trigger logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
+// Importing logic
+import calculate from '../logic/calculate';
+
 // Importing components
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-export default function App() {
-  return (
-    <div id="calculator-container" className="calculator-container">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { total: null, next: null, operation: null };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    let value;
+    if (total && operation) {
+      value = `${total} ${operation}`;
+    }
+    if (next) {
+      value = value ? `${value} ${next}` : next;
+    }
+
+    return (
+      <div id="calculator-container" className="calculator-container">
+        <Display value={value || undefined} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
