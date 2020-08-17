5bcd70ae880237ed80794039556a5910afd04eda
Juan Manuel <44899916+codingAngarita@users.noreply.github.com>
2020-08-17 02:38:11 -0500

    Displayed previous value

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,18 +3,22 @@ import PropTypes from 'prop-types';
 
 export default function Display(props) {
   const { value } = props;
+  const { previousValue } = props;
 
   return (
     <div className="display-container" id="display-container">
+      <p className="previous-calculation">{ previousValue }</p>
       <p>{ value }</p>
     </div>
   );
 }
 
 Display.defaultProps = {
+  previousValue: '',
   value: '0',
 };
 
 Display.propTypes = {
+  previousValue: PropTypes.string,
   value: PropTypes.string,
 };
